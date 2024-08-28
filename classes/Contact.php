c131995191df0d4bdb5d7dc9bdeeb09d126eaf6f
mmccjjj <dhuber88@sunrise.ch>
2024-08-28 16:55:55 +0200

    übung

--- classes/Contact.php
+++ classes/Contact.php
@@ -4,27 +4,58 @@ declare(strict_types=1);
 
 
 class Contact{
-    public string $firstname;
-    public string $lastname;
-    public string $email;
-    public string $birthdate;
-    public bool $related;
-    public int $value;
-    public string $defaultValue= 'Standartwert';
+    protected string $firstname;
+    protected string $lastname;
+    protected string $email;
+    protected DateTime $birthdate;
+    protected bool $related;
+    protected int $value;
+    protected string $defaultValue= 'Standartwert';
 
     function __construct($contactArrayInput)
     {
         $this->firstname = $contactArrayInput['firstname'];
         $this->lastname = $contactArrayInput['lastname']; 
-        $this->email = $contactArrayInput['email']; 
-        $this->birthdate = $contactArrayInput['birthdate']; 
+        $this->setEmail($contactArrayInput['email']);
+        $this->setBirthdate($contactArrayInput['birthdate']);
         $this->related = $contactArrayInput['related']; 
-        $this->value = $contactArrayInput['value'];    
+        $this->value = $contactArrayInput['value'];
+
     }
 
     function getFullname(){
         $fullname= $this->firstname." ".$this->lastname;
         return $fullname;
+    }
+
+    function notifyByEmail($subject, $message){
+        return 'Betreff: '.$subject.'mail: '.$this->email.': '.'Hallo '.$this->getFullname().', '.$message.'. Freundliche Grüsse';
+    }
+
+    function setEmail($email){
+        if ((filter_var($email, FILTER_VALIDATE_EMAIL))) {
+            $this->email= $email;
+        }else{
+            $this->email= "";
+        }
+        
+    }
+
+    function setBirthdate($date){
+        $dateTimeOb= new DateTime($date);
+        if ($dateTimeOb< new DateTime()) {
+            $this->birthdate= $dateTimeOb;
+        }else{
+            $this->birthdate= "";
+        }
+
+
+    }
+
+    function getBirthdateInSwissFormat(){
+
+        return  $this->birthdate->format('d.m.Y');
+
     }
 
 }
--- index.php
+++ index.php
@@ -19,24 +19,38 @@ $contactArray= [
                     "birthdate"=> "06.06.1988",
                     "related"=> false,
                     "value"=> 4,
-                ]
+                ],
+    "person3"=> [
+                    "firstname"=> "Hans",
+                    "lastname"=> "Meyer",
+                    "email"=> "hans.meyer@opportunity-zuerich.ch",
+                    "birthdate"=> "12.12.1988",
+                    "related"=> false,
+                    "value"=> 6,
+                ],
 
 ];
-
+// hans.meyer@opportunity-zuerich.ch
 $contactPerson1= new Contact($contactArray["person1"]);
 
 
 
-$contactPerson2= new Contact($contactArray["person2"]);
+/* $contactPerson2= new Contact($contactArray["person2"]);
 
 if ($contactPerson2== $contactPerson1){
     echo "<p>Sind Gleich</p>";
 }else {
     echo "<p>Sind nicht gleich</p>";
-}
+} */
 
 
-var_dump($contactPerson2->getFullname())
+//var_dump($contactPerson2->notifyByEmail("Test", "Test Nachricht"));
+
+$contactPerson3= new Contact($contactArray["person3"]);
+
+var_dump($contactPerson3->getBirthdateInSwissFormat());
+
+
 
 ?>
 
